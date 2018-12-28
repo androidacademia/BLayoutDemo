3029a774d709ff7ed24b424adfe32a52a373bc00
Girish Kumar Shakya <clairvoyant.1947@gmail.com>
2018-12-28 13:38:33 +0530

    Initial commit

---
+++ app/src/main/java/helloworld/com/layoutdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package helloworld.com.layoutdemo;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_demo);
+
+    }
+}
